7b2f0d76f2e5371e6fddf1ef2dcbd84f84e2b6ea
oscar_mesa <oscarmesa.elpoli@gmail.com>
2020-05-28 03:47:16 -0500

    Updates

---
+++ precache-manifest.ff4fbcfd419d11552a7860cefa0ac3ab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "052293c217a11161bf35e15187c463a8",
+    "url": "/planta/index.html"
+  },
+  {
+    "revision": "1f8f9bdb510bdc33ae8b",
+    "url": "/planta/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "05887f946ae9814c83e2",
+    "url": "/planta/static/js/2.dcca4015.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/planta/static/js/2.dcca4015.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f8f9bdb510bdc33ae8b",
+    "url": "/planta/static/js/main.5c032757.chunk.js"
+  },
+  {
+    "revision": "94ab6d6b1d2d2a58d86a",
+    "url": "/planta/static/js/runtime-main.548de937.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/planta/static/media/logo.5d5d9eef.svg"
+  }
+]);
